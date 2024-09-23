9105430f543ffc4f7b96b988507d6907f1f11a4e
Danila Albutov <danil.miheev2222@gmail.com>
2024-09-23 15:57:16 -0700

    Quick_repeat.py day 1

---
+++ Day1/Quick_repeat.py
@@ -0,0 +1,14 @@
+# print("Enter to do: ")
+# user_text = input("Enter to do: ")
+#
+# print(user_text)
+user_text = "Enter to do:"
+todo1 = input(user_text)
+todo2 = input(user_text)
+todo3 = input(user_text)
+
+todos = [todo1, todo2, todo3, "- end of morning -"]
+print(todos)
+print(type(todos))
+
+print(len(todos))
