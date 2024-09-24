641edecbd103cb7061222f7d314438250c76027f
Danila Albutov <danil.miheev2222@gmail.com>
2024-09-23 17:01:49 -0700

    Quick_repeat2.py day 2

---
+++ Day2/Quick_repeat2.py
@@ -0,0 +1,40 @@
+user_prompt = "Enter a todo: "
+
+# infinite loop
+# while 2 > 1:
+# while True:
+#     todo = input(user_prompt)
+#     todos = [todo]
+#     print(todos)
+
+#  Python methods
+# .append will add the deliverables to the end of your list
+todos =[]
+
+# while True:
+#     todo = input(user_prompt)
+#     todos.append(todo)
+#     print(todos)
+
+# .capitalize will make your first letter big
+
+# while True:
+#     todo = input(user_prompt)
+#     print(todo.capitalize())
+#     todos.append(todo)
+# while True:
+#     todo = input(user_prompt)
+#     print(todo.title())
+#     todos.append(todo)
+#
+# !=
+
+# password = input("Enter a your password")
+#
+# while password != "db.user.id.password":
+#     print("Wrong Password")
+
+x = 0
+while x < 6:
+    x = x+1
+    print(x)
