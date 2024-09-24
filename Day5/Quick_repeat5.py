3314d45b004b0cddaa79cc018efa27fabaf14699
Danila Albutov <danil.miheev2222@gmail.com>
2024-09-23 23:16:06 -0700

    Quick_repeat5.py day 5

--- Day5/Quick_repeat5.py
+++ Day5/Quick_repeat5.py
@@ -1,16 +1,17 @@
+# Enumerate function
 todos = []
 
 while True:
-    user_action = input("Type add , or show/display, or edit, or exit: ")
+    user_action = input("Type add , or show/display, or edit,or complete, or exit: ")
     user_action = user_action.strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show" | 'display':
-          for item in todos:
+          for index,item in enumerate(todos):
               item = item.title()
-              print(item)
+              print(f"{index+1}.{item}")
 
         case "edit":
             number = int(input("Enter a number of todo task that you want to edit: "))
@@ -19,9 +20,10 @@ while True:
             todos[number] = new_todo
             # existing_todo = todos[number]
             # print(todos[number])
+        case "complete":
+            number = int(input("number of the todo that complete: "))
+            todos.pop(number-1)
         case "exit":
             break
         case _:
             print("hey,you entered an unknown command")
-
-
